7516053ab03b761c547b5b9b5982cdbb01bb8eb7
megsgaard <mathias.egsgaard@post.au.dk>
2019-02-11 10:59:30 +0100

    Tilføjet et refactored projekt, hvor der er sat interfaces imellem controller og componenter.

---
+++ ECS.Refactored/Application.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Refactored
+{
+   public class Application
+   {
+      static void Main(string[] args)
+      {
+         IRegulator regulator = new Heater();
+         ISensor sensor = new TempSensor();
+
+         var ecs = new ECS(28, sensor, regulator);
+
+         ecs.Regulate();
+
+         ecs.SetThreshold(20);
+
+         ecs.Regulate();
+
+         Console.ReadKey();
+      }
+   }
+}
---
+++ ECS.Refactored/ECS.cs
@@ -0,0 +1,46 @@
+﻿namespace ECS.Refactored
+{
+    public class ECS
+    {
+        private int _threshold;
+        private readonly ISensor _tempSensor;
+        private readonly IRegulator _heater;
+
+        public ECS(int thr, ISensor tempSensor, IRegulator heater)
+        {
+           _tempSensor = tempSensor;
+           _heater = heater;
+           SetThreshold(thr);
+        }
+
+        public void Regulate()
+        {
+            var t = _tempSensor.GetTemp();
+            if (t < _threshold)
+                _heater.TurnOn();
+            else
+                _heater.TurnOff();
+
+        }
+
+        public void SetThreshold(int thr)
+        {
+            _threshold = thr;
+        }
+
+        public int GetThreshold()
+        {
+            return _threshold;
+        }
+
+        public int GetCurTemp()
+        {
+            return _tempSensor.GetTemp();
+        }
+
+        public bool RunSelfTest()
+        {
+            return _tempSensor.RunSelfTest() && _heater.RunSelfTest();
+        }
+    }
+}
